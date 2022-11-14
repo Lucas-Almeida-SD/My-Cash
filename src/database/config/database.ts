e5b7832555f93d2064aab65a5eaafda50ab29279
lucas_almeida <lucas.almdiniz@gmail.com>
2022-11-14 17:45:10 -0300

    chore: adicionado sufixo no nome do database

--- src/database/config/database.ts
+++ src/database/config/database.ts
@@ -7,13 +7,16 @@ const {
   POSTGRE_SQL_HOST,
   POSTGRE_SQL_DB,
   POSTGRE_SQL_PORT,
+  NODE_ENV,
 } = process.env;
+
+const DB_NAME_SUFIX = (NODE_ENV) ? `-${NODE_ENV}` : '-dev';
 
 const config: Options = {
   username: POSTGRE_SQL_USERNAME || 'postgres',
   password: POSTGRE_SQL_PASSWORD || 'password',
   host: POSTGRE_SQL_HOST || 'localhost',
-  database: POSTGRE_SQL_DB || 'ng_cash',
+  database: `${POSTGRE_SQL_DB || 'ng_cash'}${DB_NAME_SUFIX}`,
   port: Number(POSTGRE_SQL_PORT) || 5432,
   dialect: 'postgres',
 };
