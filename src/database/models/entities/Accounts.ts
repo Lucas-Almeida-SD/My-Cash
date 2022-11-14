c8f4b0b62b19bb46fc2635a857a8f5b6102b42a0
lucas_almeida <lucas.almdiniz@gmail.com>
2022-11-14 20:53:58 -0300

    feat: criada classe Accounts
    representa a entidade Accounts na forma de objeto

---
+++ src/database/models/entities/Accounts.ts
@@ -0,0 +1,32 @@
+import { Model, INTEGER } from 'sequelize';
+import db from '../..';
+
+class Accounts extends Model {
+  id!: number;
+
+  balance!: number;
+}
+
+Accounts.init(
+  {
+    id: {
+      type: INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+      unique: true,
+    },
+    balance: {
+      type: INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: false,
+    underscored: true,
+    sequelize: db,
+    tableName: 'Accounts',
+  },
+);
+
+export default Accounts;
