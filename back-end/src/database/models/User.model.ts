5228bc58c8afd1e826dadf4522d5e615fa629062
lucas_almeida <lucas.almdiniz@gmail.com>
2022-11-15 01:41:33 -0300

    feat: criado método create na camada model da rota "POST /users"

--- back-end/src/database/models/User.model.ts
+++ back-end/src/database/models/User.model.ts
@@ -1,16 +1,29 @@
-import { IUserModel, IUser, IUserLoginRequest } from '../../interfaces/IUser.interface';
+import { Transaction } from 'sequelize';
+import {
+  IUserModel, IUser, IUserRequest, IUserCreate,
+} from '../../interfaces/IUser.interface';
 import Users from './entities/Users';
 
 export default class UserModel implements IUserModel {
   constructor(private entity = Users) {}
 
-  public async login(userLogin: IUserLoginRequest): Promise<IUser | null> {
+  public async login(userLogin: IUserRequest): Promise<IUser | null> {
     const { username } = userLogin;
 
     const user = await this.entity.findOne({
       where: {
         username,
       },
+    });
+
+    return user;
+  }
+
+  public async create(newUser: IUserCreate, transaction: Transaction): Promise<IUser> {
+    const user = await this.entity.create({
+      ...newUser,
+    }, {
+      transaction,
     });
 
     return user;
