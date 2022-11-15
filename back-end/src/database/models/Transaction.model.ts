9716ce2f632951af18ed62c09adbe2b09127239b
lucas_almeida <lucas.almdiniz@gmail.com>
2022-11-15 19:58:31 -0300

    feat: criado método getMyTransactions na camada model da rota "GET /transactions/me"

--- back-end/src/database/models/Transaction.model.ts
+++ back-end/src/database/models/Transaction.model.ts
@@ -1,5 +1,7 @@
-import { Transaction } from 'sequelize';
-import { ITransactionCreate, ITransactionModel } from '../../interfaces/ITransaction.interface';
+import { Transaction, Op } from 'sequelize';
+import {
+  ITransaction, ITransactionCreate, ITransactionModel, ITransactionFilterOptions,
+} from '../../interfaces/ITransaction.interface';
 import Transactions from './entities/Transactions';
 
 export default class TransactionModel implements ITransactionModel {
@@ -10,5 +12,30 @@ export default class TransactionModel implements ITransactionModel {
     transactionSequelize: Transaction,
   ): Promise<void> {
     await this.entity.create({ ...newTransaction }, { transaction: transactionSequelize });
+  }
+
+  public async getMyTransactions(
+    accountId: number,
+    filterOptions: ITransactionFilterOptions,
+  ): Promise<[] | ITransaction[]> {
+    const { createdAt, cashType } = filterOptions;
+    const where = {} as any;
+
+    if (createdAt) where.createdAt = createdAt;
+    if (cashType) {
+      where[(cashType === 'out') ? 'debitedAccountId' : 'creditedAccountId'] = accountId;
+    } else {
+      where[Op.or] = [
+        { debitedAccountId: accountId },
+        { creditedAccountId: accountId },
+      ];
+    }
+
+    const transactions = await this.entity.findAll({
+      where,
+      order: [['createdAt', 'DESC']],
+    });
+
+    return transactions;
   }
 }
