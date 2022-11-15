f4ee5e337b2d129c0076473942bdba946722e4f0
lucas_almeida <lucas.almdiniz@gmail.com>
2022-11-15 01:58:05 -0300

    feat: função que gera o hash do password

---
+++ back-end/src/helpers/generateHashPassword.ts
@@ -0,0 +1,11 @@
+import bcrypt from 'bcrypt';
+
+function generateHashPassword(password: string): string {
+  const saltRounds = 10;
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hash = bcrypt.hashSync(password, salt);
+
+  return hash;
+}
+
+export default generateHashPassword;
