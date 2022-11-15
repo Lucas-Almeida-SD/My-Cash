926de3473e7d442be818b92131502e4c28b373ac
lucas_almeida <lucas.almdiniz@gmail.com>
2022-11-14 22:09:44 -0300

    feat: adicionada validação dos campos de login no método "login"

--- back-end/src/services/User.service.ts
+++ back-end/src/services/User.service.ts
@@ -1,16 +1,21 @@
+import { StatusCodes } from 'http-status-codes';
 import UserModel from '../database/models/User.model';
 import generateToken from '../helpers/generateToken';
-import { IUserLoginRequest, IUserService } from '../interfaces/IUser.interface';
+import throwMyError from '../helpers/throwMyError';
+import { IUser, IUserLoginRequest, IUserService } from '../interfaces/IUser.interface';
+import UserValidation from '../validations/User.validation';
 
 export default class UserService implements IUserService {
   constructor(private model: UserModel) {}
 
   public async login(userLogin: unknown): Promise<string> {
+    UserValidation.loginValidate(userLogin);
+
     const user = await this.model.login(userLogin as IUserLoginRequest);
 
-    if (!user) throw new Error('Not found');
+    if (!user) throwMyError(StatusCodes.NOT_FOUND, 'Usuário não encontrado');
 
-    const token = generateToken(user);
+    const token = generateToken(user as IUser);
 
     return token;
   }
