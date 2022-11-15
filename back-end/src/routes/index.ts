17519a71e2bdccc4d59d365fff7a7d2354e15cfc
lucas_almeida <lucas.almdiniz@gmail.com>
2022-11-15 16:47:53 -0300

    chore: transactiosRouter adicionado

--- back-end/src/routes/index.ts
+++ back-end/src/routes/index.ts
@@ -1,7 +1,9 @@
 import usersRouter from './users.routes';
 import accountRouter from './accounts.routes';
+import transactiosRouter from './transactions.routes';
 
 export default {
   usersRouter,
   accountRouter,
+  transactiosRouter,
 };
