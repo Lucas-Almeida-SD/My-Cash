541ff69d8a94245eb19a5ece618eed065f3ff8a0
lucas_almeida <lucas.almdiniz@gmail.com>
2022-11-15 17:12:54 -0300

    feat: criado método getByUsername na camada controller da rota "GEt users/me"

--- back-end/src/controllers/User.controller.ts
+++ back-end/src/controllers/User.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
-import { IUserController } from '../interfaces/IUser.interface';
+import { IUserController, IUserWithoutPassword } from '../interfaces/IUser.interface';
 import UserService from '../services/User.service';
 
 export default class UserController implements IUserController {
@@ -16,5 +16,11 @@ export default class UserController implements IUserController {
     const createdUser = await this.service.create(req.body);
 
     res.status(StatusCodes.CREATED).json(createdUser);
+  };
+
+  public getByUsername = async (req: Request, res: Response): Promise<void> => {
+    const getUser = await this.service.getByUsername(req.user as IUserWithoutPassword);
+
+    res.status(StatusCodes.OK).json(getUser);
   };
 }
