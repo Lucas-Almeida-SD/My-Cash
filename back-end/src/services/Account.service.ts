562d1b95745476807f419afb12887f7d208542fe
lucas_almeida <lucas.almdiniz@gmail.com>
2022-11-15 03:32:01 -0300

    chore: retirada validação do accountId pois não será necessário

--- back-end/src/services/Account.service.ts
+++ back-end/src/services/Account.service.ts
@@ -2,14 +2,11 @@ import { StatusCodes } from 'http-status-codes';
 import AccountModel from '../database/models/Account.model';
 import throwMyError from '../helpers/throwMyError';
 import { IAccount, IAccountService } from '../interfaces/IAccount.interface';
-import AccountValidation from '../validations/Account.validation';
 
 export default class AccountService implements IAccountService {
   constructor(private service: AccountModel) {}
 
   public async getById(id: number): Promise<IAccount> {
-    AccountValidation.accountIdValidate(id);
-
     const account = await this.service.getById(id);
 
     if (!account) throwMyError(StatusCodes.NOT_FOUND, 'Conta não encontrada');
