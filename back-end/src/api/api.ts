08c0d2604cb79848ba1c075c74c61d0122c3fcb2
lucas_almeida <lucas.almdiniz@gmail.com>
2022-11-14 22:12:36 -0300

    feat: adicionado middleware de erro "errorMiddleware" à API

--- back-end/src/api/api.ts
+++ back-end/src/api/api.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import routes from '../routes';
+import errorMiddleware from '../middlewares/errorMiddleware';
 
 const api = express();
 api.use(express.json());
 api.use(cors());
 
 api.use(routes.usersRouter);
+
+api.use(errorMiddleware);
 
 export default api;
