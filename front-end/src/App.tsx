c05527b7abf4da3e8f3133f598ec55c76dcb99ae
lucas_almeida <lucas.almdiniz@gmail.com>
2022-11-16 20:38:21 -0300

    feat: adicionado componente Toaster do react-hot-toast

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import Header from './components/Header';
+import routes from './helpers/routes';
+import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
-      <h1>Ola</h1>
+      <Header />
+      <Routes>
+        <Route path={routes.home} element={<Home />} />
+        <Route path={routes.login} element={<Login />} />
+      </Routes>
+      <Toaster />
     </div>
   );
 }
