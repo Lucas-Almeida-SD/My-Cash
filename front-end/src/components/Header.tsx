154f45d05cbc93d1503a084887b8d03d35757db1
lucas_almeida <lucas.almdiniz@gmail.com>
2022-11-16 02:39:54 -0300

    feat: criado componente Header

---
+++ front-end/src/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import routes from '../helpers/routes';
+
+export default function Header() {
+  return (
+    <header>
+      <div>Logo</div>
+      <nav>
+        <Link to={routes.home}>Home</Link>
+        <Link to={routes.login}>Login</Link>
+        <Link to={routes.register}>Cadastrar</Link>
+      </nav>
+    </header>
+  );
+}
