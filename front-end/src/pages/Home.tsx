517ab5175ff2ae9156a716ed4c5d7d4c981d01ec
lucas_almeida <lucas.almdiniz@gmail.com>
2022-11-18 01:29:07 -0300

    style: adicionado classnames aos elementos

--- front-end/src/pages/Home.tsx
+++ front-end/src/pages/Home.tsx
@@ -3,13 +3,13 @@ import chellphoneImg from '../assets/imgs/cellphone.png';
 
 export default function Home() {
   return (
-    <main>
-      <section>
-        <div>
+    <main id="home-page">
+      <section className="info-section">
+        <div className="content-container">
           <h2>A CARTEIRA DA NOVA GERAÇÃO.</h2>
           <p>É para todas as idades!</p>
         </div>
-        <div>
+        <div className="img-container">
           <img src={chellphoneImg} alt="Smartphones exibindo detalhes da carteira digital" />
         </div>
       </section>
