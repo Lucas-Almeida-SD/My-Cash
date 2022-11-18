d48323eb0524a299fc1509e048a9299adac29e0d
lucas_almeida <lucas.almdiniz@gmail.com>
2022-11-17 21:25:08 -0300

    feat: criado componente TransactionTable
    tabela de transações do usuario

---
+++ front-end/src/components/TransactionTable.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Transaction } from '../@types/Transaction';
+import formatToBrazilianCurrency from '../helpers/formatToBrazilianCurrency';
+import formatDate from '../helpers/formatDate';
+import useMyContext from '../hooks/useMyContext';
+
+interface Props {
+  transactions: Transaction[];
+}
+
+export default function TransactionTable({ transactions }: Props) {
+  const { user } = useMyContext();
+
+  return (
+    <table border={1}>
+      <thead>
+        <tr>
+          <th>Descrição</th>
+          <th>Data</th>
+          <th>Valor</th>
+        </tr>
+      </thead>
+      <tbody>
+        {transactions.map((transaction) => (
+          <tr key={`transaction-${transaction.id}`}>
+            <td>{`Pix ${(transaction.debitedAccountId === user.id) ? 'enviado' : 'recebido'}`}</td>
+            <td>{`${formatDate(transaction.createdAt)}`}</td>
+            <td>{formatToBrazilianCurrency(transaction.value)}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
