b929c9d5c7b5cc4a7b0e0c47b1b422e40f59507e
lucas_almeida <lucas.almdiniz@gmail.com>
2022-11-18 00:05:20 -0300

    style: adicionado classnames nos elementos

--- front-end/src/components/Header.tsx
+++ front-end/src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import classnames from 'classnames';
 import { User } from '../@types/User';
 import routes from '../helpers/routes';
 import useMyContext from '../hooks/useMyContext';
@@ -7,27 +8,55 @@ import useMyContext from '../hooks/useMyContext';
 export default function Header() {
   const { user, setUser } = useMyContext();
   const navigate = useNavigate();
+  const { pathname } = useLocation();
 
   useEffect(() => {
     if (!Object.keys(user).length) navigate(routes.home);
   }, [user]);
 
   return (
-    <header>
-      <div>Logo</div>
-      <nav>
+    <header className="page-header">
+      <div className="logo-container">
+        <h1>MY CA$H</h1>
+      </div>
+      <nav className="page-links">
         {(!Object.keys(user).length)
           ? (
             <>
-              <Link to={routes.home}>Home</Link>
-              <Link to={routes.login}>Login</Link>
-              <Link to={routes.register}>Cadastrar</Link>
+              <Link
+                to={routes.home}
+                className={classnames({ selected: pathname === routes.home })}
+              >
+                Home
+              </Link>
+              <Link
+                to={routes.login}
+                className={classnames({ selected: pathname === routes.login })}
+              >
+                Login
+              </Link>
+              <Link
+                to={routes.register}
+                className={classnames({ selected: pathname === routes.register })}
+              >
+                Cadastrar
+              </Link>
             </>
           )
           : (
             <>
-              <Link to={routes.myAccount}>Minha conta</Link>
-              <Link to={routes.transactions}>Transações</Link>
+              <Link
+                to={routes.myAccount}
+                className={classnames({ selected: pathname === routes.myAccount })}
+              >
+                Minha conta
+              </Link>
+              <Link
+                to={routes.transactions}
+                className={classnames({ selected: pathname === routes.transactions })}
+              >
+                Transações
+              </Link>
               <Link to="/" onClick={() => setUser({} as User)}>Sair</Link>
             </>
           )}
