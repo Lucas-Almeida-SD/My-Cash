fe2718051f9ce5ebec857c813151add0c00ebf28
lucas_almeida <lucas.almdiniz@gmail.com>
2022-11-17 21:18:48 -0300

    feat: adicionada condiçao para exibir links de navegação diferentes quando user logar

--- front-end/src/components/Header.tsx
+++ front-end/src/components/Header.tsx
@@ -1,15 +1,36 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { User } from '../@types/User';
 import routes from '../helpers/routes';
+import useMyContext from '../hooks/useMyContext';
 
 export default function Header() {
+  const { user, setUser } = useMyContext();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!Object.keys(user).length) navigate(routes.home);
+  }, [user]);
+
   return (
     <header>
       <div>Logo</div>
       <nav>
-        <Link to={routes.home}>Home</Link>
-        <Link to={routes.login}>Login</Link>
-        <Link to={routes.register}>Cadastrar</Link>
+        {(!Object.keys(user).length)
+          ? (
+            <>
+              <Link to={routes.home}>Home</Link>
+              <Link to={routes.login}>Login</Link>
+              <Link to={routes.register}>Cadastrar</Link>
+            </>
+          )
+          : (
+            <>
+              <Link to={routes.myAccount}>Minha conta</Link>
+              <Link to={routes.transactions}>Transações</Link>
+              <Link to="/" onClick={() => setUser({} as User)}>Sair</Link>
+            </>
+          )}
       </nav>
     </header>
   );
