96ef254f6d26f7fd6a9993dda32ec72f4b5a46fd
lucas_almeida <lucas.almdiniz@gmail.com>
2022-11-19 03:12:10 -0300

    feat: renderiza os icones de transação

--- front-end/src/components/TransactionTable.tsx
+++ front-end/src/components/TransactionTable.tsx
@@ -3,6 +3,8 @@ import { Transaction } from '../@types/Transaction';
 import formatToBrazilianCurrency from '../helpers/formatToBrazilianCurrency';
 import formatDate from '../helpers/formatDate';
 import useMyContext from '../hooks/useMyContext';
+import receiveMoneyIcon from '../assets/imgs/receive-money.png';
+import spentMoneyIcon from '../assets/imgs/spent-money.png';
 
 interface Props {
   transactions: Transaction[];
@@ -18,16 +20,29 @@ export default function TransactionTable({ transactions }: Props) {
           <th>Descrição</th>
           <th>Data</th>
           <th>Valor</th>
+          <th>Tipo</th>
         </tr>
       </thead>
       <tbody>
-        {transactions.map((transaction) => (
-          <tr key={`transaction-${transaction.id}`}>
-            <td>{`Pix ${(transaction.debitedAccountId === user.id) ? 'enviado' : 'recebido'}`}</td>
-            <td>{`${formatDate(transaction.createdAt)}`}</td>
-            <td>{formatToBrazilianCurrency(transaction.value)}</td>
-          </tr>
-        ))}
+        {transactions.map((transaction) => {
+          const { debitedAccountId } = transaction;
+          const transactionType = (debitedAccountId === user.id) ? 'enviado' : 'recebido';
+          const transactionSymbol = (transactionType === 'enviado') ? '-' : '+';
+
+          return (
+            <tr key={`transaction-${transaction.id}`}>
+              <td>{`Pix ${transactionType}`}</td>
+              <td>{`${formatDate(transaction.createdAt)}`}</td>
+              <td>{`${transactionSymbol} ${formatToBrazilianCurrency(transaction.value)}`}</td>
+              <td>
+                <img
+                  src={(transactionType === 'recebido') ? receiveMoneyIcon : spentMoneyIcon}
+                  alt="Tipo de transferência realizada"
+                />
+              </td>
+            </tr>
+          );
+        })}
       </tbody>
     </table>
   );
