c14443bc2a3b887703dafe77afb00b9122274ca1
lucas_almeida <lucas.almdiniz@gmail.com>
2022-11-19 21:47:59 -0300

    feat: criado componente Loading

---
+++ front-end/src/components/Loading.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Loading() {
+  return (
+    <div id="loading">
+      <div className="loading-circle-1" />
+      <div className="loading-circle-2" />
+    </div>
+  );
+}
