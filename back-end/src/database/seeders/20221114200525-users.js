79e9ea1a5c344a73bf099d4e1d4a4e11fc9676d7
lucas_almeida <lucas.almdiniz@gmail.com>
2022-11-20 01:25:05 -0300

    chore: alterado valor de username

--- back-end/src/database/seeders/20221114200525-users.js
+++ back-end/src/database/seeders/20221114200525-users.js
@@ -3,12 +3,12 @@ module.exports = {
   async up(queryInterface, _Sequelize) {
     await queryInterface.bulkInsert('Users', [
       {
-        username: '@john_doe',
+        username: 'john_doe',
         password: '$2b$10$CT0b4TfrqSbSNiRnERBkO.16B2ens.bvOwC6nBjtx61ig8A374vGq', // Senha123
         account_id: 1,
       },
       {
-        username: '@joao_santos',
+        username: 'joao_santos',
         password: '$2b$10$UEDfiVuHkCEnssSL7H7d9eR6UXKs/TSg7tx1aKtHOkk/PUqdn5DEO', // 123Senha
         account_id: 2,
       },
