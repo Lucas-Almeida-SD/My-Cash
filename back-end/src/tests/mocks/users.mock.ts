f3ed0b62a6647731cfcfc778fb5683f5ff613737
lucas_almeida <lucas.almdiniz@gmail.com>
2022-11-20 02:15:21 -0300

    test: adicionado mocks para os testes da rota "POST /users"

--- back-end/src/tests/mocks/users.mock.ts
+++ back-end/src/tests/mocks/users.mock.ts
@@ -1,3 +1,4 @@
+// Login
 export const successUsersLoginRequest = {
   username: 'john_doe',
   password: 'Senha123'
@@ -19,4 +20,43 @@ export const notFoundUsernameUsersLoginRequest = {
 export const incorrectPasswordUsersLoginRequest = {
   username: 'john_doe',
   password: 'IncorrectPassword'
+}
+
+// Create a new user
+export const successUsersCreateRequest = {
+  username: 'new_user',
+  password: 'Password1'
+}
+
+export const nonExistentUsernameUsersCreateRequest = {
+  password: 'Password1'
+}
+
+export const usernameLessThanThreeCharactersUsersCreateRequest = {
+  username: 'ne',
+  password: 'Password1'
+}
+
+export const nonExistentPasswordUsersCreateRequest = {
+  username: 'new_user',
+}
+
+export const passwordLessThanEightCharactersUsersCreateRequest = {
+  username: 'new_user',
+  password: 'Pass123'
+}
+
+export const passwordWithoutAtLeastOneUppercaseLetterUsersCreateRequest = {
+  username: 'new_user',
+  password: 'password1'
+}
+
+export const passwordWithoutAtLeastOneNumberUsersCreateRequest = {
+  username: 'new_user',
+  password: 'passwordd'
+}
+
+export const userAlreadyExistsUsersCreateRequest = {
+  username: 'john_doe',
+  password: 'Password1'
 }
