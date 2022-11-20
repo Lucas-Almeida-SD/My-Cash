c67f2c63d1ef0531ae27f262b0890a029ce9172f
lucas_almeida <lucas.almdiniz@gmail.com>
2022-11-19 21:49:39 -0300

    feat: renderiza componente Loading
    renderiza componente Loading quando a aplicação estiver fazendo alguma requisição

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
-import Header from './components/Header';
+import useMyContext from './hooks/useMyContext';
 import routes from './helpers/routes';
+import Header from './components/Header';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import MyAccount from './pages/MyAccount';
 import Transactions from './pages/Transactions';
+import Loading from './components/Loading';
 import './styles/global.scss';
 
 function App() {
+  const { isLoading } = useMyContext();
+
   return (
     <div className="App">
       <Header />
+      {(isLoading) && <Loading />}
       <Routes>
         <Route path={routes.home} element={<Home />} />
         <Route path={routes.login} element={<Login />} />
