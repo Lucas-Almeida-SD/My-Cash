10fc797f8f561ee85161ee0dcda5399328e279f3
lucas_almeida <lucas.almdiniz@gmail.com>
2022-11-19 22:42:14 -0300

    chore: diretório apagado

--- back-end/src/api/server.ts
+++
@@ -1,8 +0,0 @@
-import api from './api';
-import 'dotenv/config';
-
-const PORT = Number(process.env.PORT) || 3001;
-
-api.listen(PORT, () => {
-  console.log(`Running server on port: ${PORT}`);
-});
--- back-end/src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import usersRouter from './users.routes';
-import accountRouter from './accounts.routes';
-import transactiosRouter from './transactions.routes';
-
-export default {
-  usersRouter,
-  accountRouter,
-  transactiosRouter,
-};
